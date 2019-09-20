6af03f7aaae6ff770d1ca929562d64f14c817215
Jimmy Kim (CW) <jimkim@starbucks.com>
2019-09-20 11:15:25 -0700

    updates to add shopping list

---
+++ server/src/resolvers/Query.js
@@ -0,0 +1,46 @@
+async function feed(parent, args, context) {
+  const count = await context.prisma
+    .linksConnection({
+      where: {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter },
+        ],
+      },
+    })
+    .aggregate()
+    .count()
+  const links = await context.prisma.links({
+    where: {
+      OR: [
+        { description_contains: args.filter },
+        { url_contains: args.filter },
+      ],
+    },
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy,
+  })
+  return {
+    count,
+    links
+  }
+}
+
+async function profiles(parent, args, context){
+  const users = await context.prisma.users({
+    where: {
+      OR: [
+        { description_contains: args.filter },
+      ],
+    },
+  })
+  return {
+    users
+  }
+}
+
+module.exports = {
+  feed,
+  profiles
+}
